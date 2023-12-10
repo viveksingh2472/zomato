f3739e118d19c60cc868e308f04108b5accf3191
viveksingh2472 <viveksingh2472@gmail.com>
2023-12-10 21:11:23 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import area (optional)
+
+import { useState } from "react";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./components/pageNotFound";
+import Restaurant from "./components/Restaurant";
+
+const App = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search/:meal_id/:meal_type_name" element={<Search />} />
+          <Route path="/restaurant/:id" element={<Restaurant />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
+// export area --> export function
+
+// module.exports = App
+export default App;
+
+// component lifecycle
+// component
+// initialization --> component get memory
+// rendering  ---> JSX runs
+
+// load --> mounting (once)
+// update ---> updating (multiple time)
+// unload ---> unmounting (once)
+
+// functional component --> hook --> useEffect
+// useEffect happen
+// human ---> birth (once) ---> progress (updating) ---> die (once)
